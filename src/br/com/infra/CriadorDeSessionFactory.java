4bc92246909a03ef524d2fbda4d00f847fb63304
heitor <heitorguerrac@gmail.com>
2015-10-19 17:18:15 -0200

    ApplicationScoped

--- src/br/com/infra/CriadorDeSessionFactory.java
+++ src/br/com/infra/CriadorDeSessionFactory.java
@@ -1,22 +1,40 @@
 package br.com.infra;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.ioc.ComponentFactory;
 
 @Component
+@ApplicationScoped
 public class CriadorDeSessionFactory implements ComponentFactory<SessionFactory> {
+	private SessionFactory factory;
 
-	@Override
-	public SessionFactory getInstance() {
+	@PostConstruct
+	public void abre() {
 		Configuration configuration = new Configuration().configure();
 		StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
 		serviceRegistryBuilder.applySettings(configuration.getProperties());
 		ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
-		return configuration.buildSessionFactory(serviceRegistry);
+
+		this.factory = configuration.buildSessionFactory(serviceRegistry);
+	}
+
+	@Override
+	public SessionFactory getInstance() {
+		return factory;
+	}
+
+	@PreDestroy
+	public void fecha() {
+		this.factory.close();
 	}
 
 }
